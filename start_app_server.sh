503ecd70852cd39458d2b9c576efca6b26f01ec0
Sim Hua Soon <huasoon@akelesconsulting.com>
2018-10-03 16:59:30 +0800

    removed secret

--- start_app_server.sh
+++ start_app_server.sh
@@ -2,7 +2,6 @@
 # README
 
 # READ SECRET!
-export MONGODB_CREDENTIALS=mongodb://govstack2018:govstack2018@ds121203.mlab.com:21203/myinfo
 ### mongodb://{{USER}}:{{PASSWORD}}@ds666666.mlab.com:39999/some-database
 
 # finally, run the app in dev mode...
