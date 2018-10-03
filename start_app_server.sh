dda54ad8c4152265f468ba71c45177dd703e03cf
Sim Hua Soon <huasoon@akelesconsulting.com>
2018-10-03 16:25:50 +0800

    added hardcoded mongo credentials

--- start_app_server.sh
+++ start_app_server.sh
@@ -2,7 +2,8 @@
 # README
 
 # READ SECRET!
-export MONGODB_CREDENTIALS=mongodb://{{USER}}:{{PASSWORD}}@ds666666.mlab.com:39999/some-database
+export MONGODB_CREDENTIALS=mongodb://govstack2018:govstack2018@ds121203.mlab.com:21203/myinfo
+### mongodb://{{USER}}:{{PASSWORD}}@ds666666.mlab.com:39999/some-database
 
 # finally, run the app in dev mode...
 npm run dev
